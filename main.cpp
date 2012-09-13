d8d4ccb469fc523bc8d6e1de5cfabaed8192a334
Михаил Котельников <itcs@fox-info.fox.perm.ru>
2012-09-13 17:42:35 +0600

    + Установка палитры в QApplication

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+		QPalette p = a.palette();
+		p.setColor(QPalette::Window, "white");
+		p.setColor(QPalette::Button, *(new QColor(223, 128, 38)));
+		p.setColor(QPalette::ButtonText, "white");
+		a.setPalette(p);
     MainWindow w;
 		w.showFullScreen();
 
