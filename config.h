7159349d4c87024ea9d62af55c138e0b872079e0
Михаил Котельников <itcs@fox-info.fox.perm.ru>
2012-10-24 18:15:35 +0600

    v Инклады

--- config.h
+++ config.h
@@ -1,6 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <QApplication>
+#include <QString>
+
 #define DEF_CONFIG QApplication::applicationDirPath() + "/terminal.conf"
 #define DEF_CURRENCY QString::fromUtf8("руб.")
 #define DEF_HELPER QApplication::applicationDirPath() + "/helper/helper.py"
